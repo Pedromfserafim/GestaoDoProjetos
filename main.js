d97fe5b106a630f76a7a5145b6ea93463a0cfc6a
Pedromfserafim <Pedromfserafim@users.noreply.github.com>
2026-06-06 22:30:56 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,20 @@
+
 const btn = document.getElementById("botao_03");
+var estado = "AZUL";
+
+document.addEventListener('DOMContentLoaded', () => {
+    btn.style.backgroundColor = '#0d6efd';
+    estado = "AZUL";
+});
 
 btn.addEventListener("click", function() {
-    this.style.backgroundColor = '#ff5733';
+    // Usando === para comparar
+    if (estado === "AZUL") {
+        btn.style.backgroundColor = '#ff5733';
+        estado = "VERMELHO";
+    } else {
+        // Como só existem dois estados, podemos usar apenas 'else'
+        btn.style.backgroundColor = '#0d6efd';
+        estado = "AZUL";
+    }
 });
