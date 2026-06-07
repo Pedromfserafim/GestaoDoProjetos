7ac6f0445551a5bc0205c1d792d248909a088aef
Pedromfserafim <Pedromfserafim@users.noreply.github.com>
2026-06-07 15:04:32 +0100

    Update main.js

--- main.js
+++ main.js
@@ -2,26 +2,6 @@ import { GET } from "./services/GET.js";
 
 import { POST } from "./services/POST.js";
 
+import { DELETE } from "./services/DELETE.js";
 
-
-
-
-const btn = document.getElementById("botao_03");
-var estado = "AZUL";
-
-document.addEventListener('DOMContentLoaded', () => {
-    btn.style.backgroundColor = '#0d6efd';
-    estado = "AZUL";
-});
-
-btn.addEventListener("click", function() {
-    // Usando === para comparar
-    if (estado === "AZUL") {
-        btn.style.backgroundColor = '#ff5733';
-        estado = "VERMELHO";
-    } else {
-        // Como só existem dois estados, podemos usar apenas 'else'
-        btn.style.backgroundColor = '#0d6efd';
-        estado = "AZUL";
-    }
-});
+import { PATCH } from "./services/PATCH.js";    
